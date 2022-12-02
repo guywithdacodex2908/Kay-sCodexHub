f7e78e74796d92b0696b88338bae4b0ff7879835
guywithdacodex2908 <officialadupraise@gmail.com>
2022-12-02 10:49:59 +0100

    khay'scodexhub

---
+++ registerform.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionpane;
+    public class "Registration Form"{
+        public static void main(String args[]){
+JOptionpane.ShowMessageDialog(null, "Welcome to GroceryWorld");
+            
+
+
+https://cdn-icons.flaticon.com/png/512/1144/premium/1144760.png?token=exp=1635638079~hmac=5c3f0a3bece0edfc6242ebb370250b5c
+
+        }
+        }
